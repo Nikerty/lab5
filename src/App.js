669ee22fbfcb9589a30fd68764f7ea8635616893
Bearegor <bearegor@gmail.com>
2023-10-09 00:24:19 +0300

    ФИО и исправление ошибок

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Это тестовый проект Ведмеденко Егор Алексеевич";
 
-  console.log('This is App ');
   return (
     <div className="App">
-     <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         {greeting}
+        {greeting}
       </header>
     </div>
   );
