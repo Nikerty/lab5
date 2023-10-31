f11332406ad5130ce8c627147c7b99f70462581d
bearegor <bearegor@gmail.com>
2023-10-31 13:51:51 +0300

    hope

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 function App() {
   const greeting = "Это тестовый проект Ведмеденко Егора Алексеевича";
+  const hope = "Надеюсь всё заработает"
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {greeting}
+        {hope}
       </header>
     </div>
   );
